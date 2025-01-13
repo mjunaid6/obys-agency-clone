adf6a15b87681ef0fec67b69fca29d8437fefd49
mjunaid6 <mohmmadjunaid007@gmail.com>
2025-01-13 16:45:53 +0530

    page 1 completed

--- scriptSheet.js
+++ scriptSheet.js
@@ -27,42 +27,67 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
 
-var loaderTimeline = gsap.timeline();
+function loaderAnimation(){
+    var loaderTimeline = gsap.timeline();
 
-loaderTimeline.from(".line h1,h2,h4",{
-    y : 100,
-    opacity : 0,
-    duration : 0.5,
-    stagger : 0.15 
-})
-loaderTimeline.to(".line h2",{
-    opacity : 1,
-    animationName : "now-animation",
-})
-loaderTimeline.from("#load-count",{
-    opacity : 0,
-    onStart : function(){
-        var loaderCount = document.querySelector("#load-count>h6");
-        var count = 0;
-        var countInterval = setInterval(function(){
-            if(count >= 100) {
-                clearInterval(countInterval);
-            }
-            loaderCount.textContent = count++;
-        },25);
-    }
-})
-loaderTimeline.to("#loader-page",{
-    display : "none",
-    opacity : 0,
-    duration : 0.6,
-    delay : 2.5,
-    ease : Power4,
-})
-loaderTimeline.from("#page1",{
-    opacity : 0,
-    duration : 0.7,
-    delay : 0.2,
-    y : 1000,
-    ease : Power4,
-})
+    loaderTimeline.from(".line h1,h2,h4",{
+        y : 100,
+        opacity : 0,
+        duration : 0.5,
+        stagger : 0.15 
+    })
+    loaderTimeline.to(".line h2",{
+        opacity : 1,
+        animationName : "now-animation",
+    })
+    loaderTimeline.from("#load-count",{
+        opacity : 0,
+        onStart : function(){
+            var loaderCount = document.querySelector("#load-count>h6");
+            var count = 0;
+            var countInterval = setInterval(function(){
+                if(count >= 100) {
+                    clearInterval(countInterval);
+                }
+                loaderCount.textContent = count++;
+            },25);
+        }
+    })
+    loaderTimeline.to("#loader-page",{
+        display : "none",
+        opacity : 0,
+        duration : 0.6,
+        delay : 2.5,
+        ease : Power4,
+    })
+    loaderTimeline.from("#page1",{
+        opacity : 0,
+        duration : 0.7,
+        delay : 0.2,
+        y : 1000,
+        ease : Power4,
+    })
+    loaderTimeline.from("#nav-left",{
+        opacity : 0
+    })
+    loaderTimeline.from(".page1-line h1,h3",{
+        y : 100,
+        opacity : 0,
+        duration : 0.5,
+        stagger : 0.1 
+    })
+}
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(details){
+        gsap.to("#cursor",{
+            x : details.x,
+            y : details.y,
+        })
+    })
+
+    Shery.makeMagnet("#nav-right h3,#nav-left>svg:nth-child(1)");
+}
+
+loaderAnimation();
+cursorAnimation();
